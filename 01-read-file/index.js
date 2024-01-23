0e847e2c4cf3d7543ccfe6834bdb4aa6de71d16b
andrewk0704 <andrew.kozhenov@mail.ru>
2024-01-23 05:48:07 +0400

    feat: update task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-let way=path.join(__dirname, 'text.txt');
-let readStream=fs.createReadStream(way,'utf8');
+let readStream=fs.createReadStream(path.join(__dirname, 'text.txt'),'utf8');
 
 readStream.on('data', (chunk)=>{
     console.log(chunk);
